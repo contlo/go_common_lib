771c1006ac9c571e2328749e243616bf8fb8f765
Mukunda <mukunda@roadrunnr.in>
2016-06-22 00:05:28 +0530

    Adding supervisor copy file

---
+++ supervisor_cp.sh
@@ -0,0 +1,7 @@
+if [ "$1" == "development" ]
+then
+  sed "s/var_go_env/$1/g" $GOPATH/src/go_common_lib/config/supervisord.conf | sed 's/user=deployer//g' > supervisord.conf
+else
+  sed "s/var_go_env/$1/g" $GOPATH/src/go_common_lib/config/supervisord.conf > supervisord.conf
+fi
+
